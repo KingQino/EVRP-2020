dfb82236ab558ba6a0cd191c51f535a814cc1a75
Yinghao Qin <yh.qin@foxmail.com>
2023-05-30 13:19:01 +0100

    add MA-03 with complex local search operators

---
+++ evrp/ma_03.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+from .ga import *
+
+
+def pop_init(size, rand_rate, instance_name, num_vehicles, num_customers, capacity, demands):
+    num_randomly = int(size * rand_rate)
+    num_evenly = int(size * (1- rand_rate))
+
+    pop = []
+    if instance_name.startswith('E'):
+        init_pop_randomly = [generate_individual_randomly(num_vehicles, num_customers) for _ in range(num_randomly)]
+        pop.extend(init_pop_randomly)
+    elif instance_name.startswith('X'):
+        init_pop_randomly = [generate_individual_randomly_wisely(num_vehicles, num_customers, capacity, demands) for _ in range(num_randomly)]
+        init_pop_randomly_wisely = [sublist for sublist in init_pop_randomly if sublist] 
+        pop.extend(init_pop_randomly_wisely)
+    else:
+        raise NameError("The input argument '-n', namely, '--instance_name' is incorrect!")
+
+    init_pop_evenly = [generate_individual_evenly(num_vehicles, num_customers) for _ in range(num_evenly)]
+
+    pop.extend(init_pop_evenly)
+
+    return pop
+
+
+def run_MA(instance, seed, pop_size, n_gen, cx_prob, mut_prob, indpb, cand_ratio, rand_rate, ng_pop_rate, ng_immig_rate, result_dir, is_export_csv=True):
+    random.seed(seed)
+    
+    CANDIDATES = PriorityQueue()
+    PLAIN_CANDIDATES_SET = set()
+    CANDIATES_POOL_SIZE = int(cand_ratio * pop_size)
+
+    num_vehicles =  instance.num_of_vehicles
+    num_customers = instance.dimension - 1
+    capacity = instance.capacity
+    demands = instance.demands
+    station_list = instance.station_list
+    
+    csv_data = []
+    
+    start_time = time.process_time()
+    # population initialization
+    pop = []
+    ancester_pop = pop_init(pop_size, rand_rate, instance.name, num_vehicles, num_customers, capacity, demands)
+    pop.extend(ancester_pop)
+    
+
+    best_solution = []
+    best_cost = 0.0
+    
+    print('----------------Start of evolution----------------')
+    for gen in range(n_gen):
+        print(f'---- Generation {gen} ----')
+        
+        offspring_after_crossover = []
+        for ind1, ind2 in zip(pop[::2], pop[1::2]):
+            if random.random() < cx_prob:
+                child1, child2 = cx_partially_matched(ind1, ind2)
+                offspring_after_crossover.append(child1)
+                offspring_after_crossover.append(child2)        
+
+        offspring_after_muatation = []
+        for ind in offspring_after_crossover:
+            if random.random() < mut_prob:
+                mutant = mut_shuffle_indexes(ind, indpb)
+                offspring_after_muatation.append(mutant)
+
+        stats_num_candidates_added = 0
+        pop_pool = pop + offspring_after_crossover + offspring_after_muatation
+        for individual in pop_pool:
+            if is_capacity_feasible(individual, capacity, demands):
+                string_individual = '|'.join('-'.join(str(element) for element in sublist) for sublist in individual)
+                if string_individual not in PLAIN_CANDIDATES_SET:
+                    stats_num_candidates_added += 1
+                    PLAIN_CANDIDATES_SET.add(string_individual)
+                    optimized_individual, cost = local_search(individual, instance)
+                    CANDIDATES.push(optimized_individual, cost)
+        print(f'  Evaluated {stats_num_candidates_added} individuals')
+        candidates_size = CANDIDATES.size()
+        if candidates_size > CANDIATES_POOL_SIZE:
+            CANDIDATES.remove_elements(candidates_size - CANDIATES_POOL_SIZE)
+            candidates_size = CANDIDATES.size()
+            print('  Clear extra candidates from Candiate Pool')
+        print(f'  Candidates Pool Size: {candidates_size}')
+
+        
+        # Elites Population
+        elites = CANDIDATES.peek(1000)
+
+        # Statistical Data
+        size = len(elites)
+        fits = []
+        mean = 0
+        std  = 0.0
+        min_fit = None
+        max_fit = None
+
+        if size == 0:
+            print('  No candidates')
+        else:
+            fits = [fit for fit, ind in elites]
+            mean = sum(fits) / size
+            min_fit = min(fits)
+            max_fit = max(fits)
+
+        if size > 1:
+            std = (sum((x - mean) ** 2 for x in fits) / (size - 1)) ** 0.5
+        else:
+            std = 0.0
+        print(f'  Elites Num {size}')
+        print(f'  Min {min_fit}') # the best result of each generation
+        print(f'  Max {max_fit}')
+        print(f'  Avg {mean}')   # Reflect the direction of population evolution 
+        print(f'  Std {std}')
+        
+        min_individual = [] 
+        min_fitness = None
+        if size != 0:
+            min_fitness, min_individual = CANDIDATES.peek(1)[0]
+        print(f'  Best fitness: {min_fit}')
+        best_solution = min_individual
+        best_cost = min_fitness
+        # Write data to holders for exporting results to CSV file
+        if is_export_csv:
+            csv_row = {
+                'generation': gen,
+                'evaluated_individuals': stats_num_candidates_added,
+                'candidates_size': candidates_size,
+                'min_fitness': min_fit,
+                'max_fitness': max_fit,
+                'avg_fitness': mean,
+                'std_fitness': std,
+                'duration': elapsed_time(start_time),
+            }
+            csv_data.append(csv_row)
+    
+        # select
+        pop = []  
+        elites = [ind for fit, ind in elites[:500]]
+        elites.extend(CANDIDATES.random_elements(1000))
+        elites_without_stations = []
+        for ind in elites:
+            individual_without_station = []
+            for route in ind:
+                route_without_stations = [node for node in route if str(node) not in station_list]
+                individual_without_station.append(route_without_stations)
+            elites_without_stations.append(individual_without_station)       
+        pop.extend(elites_without_stations)
+        
+        num_left = pop_size - len(elites_without_stations)
+        random.shuffle(pop_pool)
+        individuals_from_pop_pool = pop_pool[:int(num_left * ng_pop_rate)]
+        pop.extend(individuals_from_pop_pool)
+        
+        immigrants_num = int(num_left * ng_immig_rate)
+        immigrants = pop_init(immigrants_num, rand_rate, instance.name, num_vehicles, num_customers, capacity, demands)
+        pop.extend(immigrants)
+
+
+    print('------------End of (successful) evolution------------', end='\n\n') 
+    duration = elapsed_time(start_time)
+    print(f'The Total Evolutionary Duration: {duration} for {n_gen} generations')
+    print(f'The average evolutionary time for each generation is: {duration/n_gen}')
+
+    csv_file = ''
+    if is_export_csv:
+        csv_file_name = f'{instance.name}_seed{seed}__pS{pop_size}_nG{n_gen}.csv'
+        csv_file = os.path.join(result_dir, csv_file_name) 
+        print(f'Write to file: {csv_file}')
+        make_dirs_for_file(path=csv_file)
+        if not exist(path=csv_file, overwrite=True):
+            with io.open(csv_file, 'wt', newline='') as file_object:
+                fieldnames = [
+                    'generation',
+                    'evaluated_individuals',
+                    'candidates_size',
+                    'min_fitness',
+                    'max_fitness',
+                    'avg_fitness',
+                    'std_fitness',
+                    'duration'
+                ]
+                writer = DictWriter(file_object, fieldnames=fieldnames, dialect='excel')
+                writer.writeheader()
+                for csv_row in csv_data:
+                    writer.writerow(csv_row)
+                    
+    return (best_solution, best_cost, csv_file)
+
+
---
+++ run_MA_03.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+import os
+import argparse
+
+
+from evrp.evrp_instance import EvrpInstance
+from evrp.utils import *
+from evrp.ma_03 import run_MA
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname('.')))
+DATA_DIR = os.path.join(BASE_DIR, 'evrp-benchmark-set')
+RESULT_DIR = os.path.join(BASE_DIR, 'results/ma-03/')
+
+def main():
+    parser = argparse.ArgumentParser(description='argparse testing')
+    parser.add_argument('--instance_name','-n',type=str, default = "E-n22-k4.evrp",required=True,help="a instance name")
+    parser.add_argument('--seed','-s',type=int, default=1,help='random seed')
+    parser.add_argument('--pop_size','-ps',type=int, default=10000,help='population size')
+    parser.add_argument('--n_gen','-g',type=int, default=150,help='the training generation number')
+    parser.add_argument('--cx_prob','-c',type=float, default=0.8,help='the probability of crossover')
+    parser.add_argument('--mut_prob','-m',type=float, default=0.5,help='the probability of mutation')
+    parser.add_argument('--indpb','-ind',type=float, default=0.2,help='the mutation probability for each element in the sequence')
+    parser.add_argument('--candidates_ratio','-cr',type=float, default=0.5,help='the preset size of the candite pool, a ratio of pop pool')
+    parser.add_argument('--random_rate','-rr',type=float, default=0.8,help='for the population initialization, the ratio of generating population by the random way')
+    parser.add_argument('--ng_pop_rate','-ngpr',type=float, default=0.7,help='for the next generation, the rate from the poplation pool after selecting the elites')
+    parser.add_argument('--ng_immig_rate','-ngir',type=float, default=0.3,help='for the next generation, the rate from the immigrants after selecting the elites')
+
+
+    args = parser.parse_args()
+
+    instance_name = args.instance_name 
+    seed = args.seed
+    pop_size = args.pop_size
+    n_gen = args.n_gen
+    cx_prob = args.cx_prob
+    mut_prob = args.mut_prob
+    indpb = args.indpb
+    cand_ratio = args.candidates_ratio
+    rand_rate = args.random_rate
+    ng_pop_rate = args.ng_pop_rate
+    ng_immig_rate = args.ng_immig_rate
+
+
+    file_dir = os.path.join(DATA_DIR, instance_name)
+    instance = EvrpInstance(file_dir)
+
+    best_solution, best_cost, training_file = run_MA(instance, seed, pop_size, n_gen, cx_prob, mut_prob, indpb, cand_ratio, rand_rate, ng_pop_rate, ng_immig_rate, RESULT_DIR)
+
+    df = create_dataframe(instance)
+
+
+    print(f'Excepted optimum: {instance.optimum}')
+    print(f'The optimal result cost from my MA: {best_cost}')
+    print(best_solution)
+
+    visualize(training_file)
+    visualize_routes(best_solution, df, 'MA with complex local search operators Best Solution for ' + instance.name, is_show=False, is_save=True, save_path=RESULT_DIR + instance.name.split('.')[0] + '-routes.png')
+
+
+
+def visualize(training_file):
+    df_run = pd.read_csv(training_file)
+    instance_name = os.path.basename(training_file).split('_')[0].split('.')[0] 
+    title = instance_name + ' | MA with complex local search operators'
+    plot_training_graph(df_run, title, is_show=False, is_save=True, save_path=RESULT_DIR + instance_name + '-evolution.png')
+
+
+if __name__ == '__main__':
+    main()
+    # visualize("/Users/yhq/Desktop/Code/EVRP-2020/results/E-n22-k4_seed1_nG200.csv")
+    
